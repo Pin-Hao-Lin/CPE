6a40e13dae5f0f7e3b7ff3a4071f8d3c128230b3
LinPinHao <james057503219@gmail.com>
2025-12-10 16:33:23 +0800

    modified:   2025-12-09/PF/11806.cpp 	new file:   2025-12-09/PF/p11806.pdf

--- 2025-12-09/PF/11806.cpp
+++ 2025-12-09/PF/11806.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const int MX = 500, MOD = 1000007;
+    int C[MX][MX] = {};
+    C[0][0] = 1;
+    for (int i = 1; i < MX; i++) {
+        C[i][0] = 1;
+        for (int j = 1; j < MX; j++) {
+            C[i][j] = (C[i-1][j] + C[i-1][j-1]) % MOD;
+        }
+    }
 
+    int T, N, M, K;
+    cin >> T;
+    while (T-- && cin >> N >> M >> K) {
+        int result = 0;
+        for (int i = 0; i <= 15; i++) {
+            int cnt = 0, r = N, c = M;
+            for (int j = 0; j < 4; j++) {
+                if (i & (1 << j)) {
+                    cnt++;
+                    if (j & 1) r--;
+                    else c--;
+                }
+            }
+            if (cnt & 1) result -= C[r*c][K];
+            else result += C[r*c][K];
+            result %= MOD;
+        }
+        cout << result << endl;
+    }
+}
